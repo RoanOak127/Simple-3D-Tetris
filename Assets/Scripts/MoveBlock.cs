4e218da001426e3b55110f49ab9fbc4123e39b97
Roan <RoanDominguez127@gmail.com>
2020-03-28 21:57:56 -0500

    Attempt to Move things
    Trying to get my object to move. It does it's own thing instead????

---
+++ Assets/Scripts/MoveBlock.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveBlock : MonoBehaviour
+{
+    public float speed;
+    private Rigidbody block;
+    // Start is called before the first frame update
+    void Start()
+    {
+        block = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //z aw
+        Vector3 pos = transform.position;
+
+        if (Input.GetKeyDown("w"))
+        {
+            Debug.Log("Pressed W");
+            pos.x = (transform.position.x - 1.0f);
+        }
+        if (Input.GetKey("s"))
+        {
+            pos.x = (transform.position.x + 1.0f);
+        }
+        if (Input.GetKey("d"))
+        {
+            pos.z = (transform.position.x + 1.0f);
+        }
+        if (Input.GetKey("a"))
+        {
+            pos.z = (transform.position.x - 1.0f);
+        }
+         block.AddForce(pos * speed);
+    }
+}
